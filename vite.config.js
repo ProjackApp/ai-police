a1b5b494127cc56a4fd9c5beb6a3eff0e091eced
HAI JACK <zakaria.zrs14@mail.com>
2026-01-20 17:29:59 +0700

    perbaikan build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // PENTING untuk Vercel
+  build: {
+    outDir: 'dist'
+  },
   server: {
     // listen on all network interfaces so the dev server is reachable on LAN
     host: true,
